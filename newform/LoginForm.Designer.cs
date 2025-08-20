2828d3f517b94ae00f0a47d52d834fdc99b8f820
SHIBIL HYDER <shibilshanhyder@gmail.com>
2025-08-20 15:19:19 +0530

    v1

---
+++ newform/LoginForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace newform
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AgeLabel = new System.Windows.Forms.Label();
+            this.TextName = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.AgeNumeric = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.AgeNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(151, 202);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(45, 17);
+            this.NameLabel.TabIndex = 0;
+            this.NameLabel.Text = "Name";
+            // 
+            // AgeLabel
+            // 
+            this.AgeLabel.AutoSize = true;
+            this.AgeLabel.Location = new System.Drawing.Point(151, 298);
+            this.AgeLabel.Name = "AgeLabel";
+            this.AgeLabel.Size = new System.Drawing.Size(33, 17);
+            this.AgeLabel.TabIndex = 1;
+            this.AgeLabel.Text = "Age";
+            // 
+            // TextName
+            // 
+            this.TextName.Location = new System.Drawing.Point(276, 202);
+            this.TextName.Name = "TextName";
+            this.TextName.Size = new System.Drawing.Size(148, 22);
+            this.TextName.TabIndex = 2;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(276, 385);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 31);
+            this.LoginButton.TabIndex = 4;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // AgeNumeric
+            // 
+            this.AgeNumeric.Location = new System.Drawing.Point(276, 293);
+            this.AgeNumeric.Maximum = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            this.AgeNumeric.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.AgeNumeric.Name = "AgeNumeric";
+            this.AgeNumeric.Size = new System.Drawing.Size(148, 22);
+            this.AgeNumeric.TabIndex = 5;
+            this.AgeNumeric.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1122, 654);
+            this.Controls.Add(this.AgeNumeric);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.TextName);
+            this.Controls.Add(this.AgeLabel);
+            this.Controls.Add(this.NameLabel);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.AgeNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label AgeLabel;
+        private System.Windows.Forms.TextBox TextName;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.NumericUpDown AgeNumeric;
+    }
+}
